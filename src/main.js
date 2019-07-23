31b9d87d298e8e0467a0809911bfcceacc7c776d
linqiyuan <466214222@qq.com>
2019-07-23 10:24:08 +0800

    大致设计

--- src/main.js
+++ src/main.js
@@ -3,8 +3,28 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import echarts from 'echarts'
+import axios from 'axios'
+
+
+import {
+  Row,
+  Col,
+  Tabs,
+  Table,
+  TableColumn
+} from 'element-ui'
+
+import './plugins/element.js'
 
 Vue.config.productionTip = false
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Tabs);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.prototype.$echarts = echarts;
+Vue.prototype.$ajax = axios
 
 new Vue({
   router,
